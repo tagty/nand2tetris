93e10c6c8d5e6b9115140df5dc134548e689858b
Tetsuya Taguchi <bouzu.ao@gmail.com>
2018-03-21 15:23:37 +0900

    add: Machine Language

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,43 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    @8192  // count = 8192
+    D=A
+    @count
+    M=D
+(RESET)
+    @index // index = 0
+    M=0
+(LOOP)
+    @KBD   // goto WHITE if KBD == 0
+    D=M
+    @WHITE
+    D;JEQ
+(BLACK)
+    @index // SCREEN = SCREEN + index
+    D=M
+    @SCREEN
+    A=A+D
+    M=1    // SCREEN = 1
+    @END
+    0;JMP
+(WHITE)
+    @index // SCREEN = SCREEN + index
+    D=M
+    @SCREEN
+    A=A+D
+    M=0    // SCREEN = 0
+(END)
+    @index // index++
+    MD=M+1
+    @count // goto REST if count - index == 0
+    D=D-M
+    @RESET
+    D;JEQ
+    @LOOP  // goto LOOP
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,27 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @sum   // sum=0
+    M=0
+    @R1    // count=R1
+    D=M
+    @count
+    M=D
+(LOOP)
+    @count // if (count)=0 goto END
+    D=M
+    @END
+    D;JEQ
+    @R0    // sum=sum+R0
+    D=M
+    @sum
+    M=M+D
+    @count // count-=1
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @sum   // R2=sum
+    D=M
+    @R2
+    M=D
